9011d27cc93ddc4202821f7a3fd18b69924364d8
fyap898 <124294533+fyap898@users.noreply.github.com>
2025-02-18 18:38:20 -0700

    Update server.js

--- server.js
+++ server.js
@@ -395,10 +395,24 @@ app.get('/paintings/genre/:ref', async (req, resp) => {
 
 app.get('/paintings/era/:ref', async (req, resp) => {
     try{
+        const eraId = req.params.ref;
+
+        const genres = await supabase
+        .from('genres')
+        .select('genreId')
+        .eq('eraId', eraId);
+
+        if(genres.error){
+            console.error("Error fetching era: ", err);
+            resp.status(500).send("Error fetching era");
+        }
+
+        const genreId = genres.data.map(g => g.genreId)
+
         const {data, error} = await supabase
         .from('paintinggenres')
-        .select('eras!eraId(paintingId, title, yearOfWork)')
-        .eq('genreId', req.params.ref)
+        .select('paintings!paintingId(paintingId, title, yearOfWork)')
+        .in('genreId', genreId)
         .order('paintings(yearOfWork)', {ascending: true});
 
         if(error)
@@ -411,7 +425,137 @@ app.get('/paintings/era/:ref', async (req, resp) => {
     }
 });
 
+////////////////////////////////////////////////////////////////////////////
+app.get('/counts/genres', async (req, resp) => {
+    try{
+        const {data, error} = await supabase
+        .from('paintinggenres')
+        .select('genreId', 'paintingId');
 
+        if(error){
+            console.error("Error fetching painting genres: ", error);
+            return resp.status(500).send("failure to fetch painting genres");
+        }
+
+        const paintingCount = {};
+        data.forEach(item => {
+            paintingCount[item.genreId] = (paintingCount[item.genreId] || 0) + 1;
+        });
+
+        const genreIds = Object.keys(paintingCount);   // gives an array of keys
+
+        const genreNames = await supabase
+        .from('genres')
+        .select('genreId, genreName')
+        .in('genreId', genreIds);
+
+        if (genreNames.error || !genreNames.data) {
+            console.error('Error fetching genre names:', genreNames.error);
+            return resp.status(500).send('Failed to fetch genre names');
+        }
+
+        const result = genreNames.data.map(g => ({
+            genre : g.genreName,
+            count : paintingCount[g.genreId]
+        })).sort((a,b) => a.count - b.count);
+
+        resp.send(result);
+
+        } catch(err) {
+            console.error("Unexpected error: ", err);
+            return resp.status(500).send("Unexpected error occured");
+        }
+});
+
+app.get("/counts/artists", async (req, resp) => {
+    try{
+        const {data, error} = await supabase
+        .from('paintings')
+        .select('paintingId, artistId')
+
+        if(error) {
+            console.error("Error fetching painting artists: ", error);
+            return resp.status(500).send("failure to fetch painting artists");
+        }
+
+        const paintingCount = {};
+        data.forEach(item => {
+            paintingCount[item.artistId] = (paintingCount[item.artistId] || 0) + 1;
+        });
+
+        const artistIds = Object.keys(paintingCount);
+
+        const artistNames = await supabase
+        .from('artists')
+        .select('artistId, firstName, lastName')
+        .in('artistId', artistIds);
+
+        if (artistNames.error || !artistNames.data) {
+            console.error('Error fetching artist names:', artistNames.error);
+            return resp.status(500).send('Failed to fetch artist names');
+        }
+
+        const result = artistNames.data.map(a => ({
+            first_name: a.firstName,
+            last_name: a.lastName,
+            count: paintingCount[a.artistId]
+        })).sort((a,b) => b.count - a.count);
+
+        resp.send(result);
+
+    } catch(err) {
+        console.error("Unexpected error: ", err);
+        return resp.status(500).send("Unexpected error occured");
+    }
+});
+
+////////////////////////////////////////////////////////////////////////////
+app.get('/counts/topgenres/:ref', async (req, resp) => {
+    const atLeast = req.params.ref;
+    try{
+        const {data, error} = await supabase
+        .from('paintinggenres')
+        .select('genreId', 'paintingId');
+
+        if(error){
+            console.error("Error fetching painting genres: ", error);
+            return resp.status(500).send("failure to fetch painting genres");
+        }
+
+        const paintingCount = {};
+        data.forEach(item => {
+            paintingCount[item.genreId] = (paintingCount[item.genreId] || 0) + 1;
+        });
+        const filteredCount = {};
+        data.forEach(item => {
+            if(paintingCount[item.genreId] >= atLeast)
+                filteredCount[item.genreId] = paintingCount[item.genreId];
+        })
+
+        const genreIds = Object.keys(filteredCount);
+
+        const genreNames = await supabase
+        .from('genres')
+        .select('genreId, genreName')
+        .in('genreId', genreIds);
+
+        if (genreNames.error || !genreNames.data) {
+            console.error('Error fetching genre names:', genreNames.error);
+            return resp.status(500).send('Failed to fetch genre names');
+        }
+
+        const result = genreNames.data.map(g => ({
+            genre : g.genreName,
+            count : paintingCount[g.genreId]
+        })).sort((a,b) => b.count - a.count);
+
+        resp.send(result);
+
+        } catch(err) {
+            console.error("Unexpected error: ", err);
+            return resp.status(500).send("Unexpected error occured");
+        }
+});
 
 app.listen(8080, () => {
     console.log('listening on port 8080');
@@ -443,5 +587,9 @@ app.listen(8080, () => {
 
     console.log('http://localhost:8080/paintings/genre/78');
     console.log('http://localhost:8080/paintings/era/2');
+
+    console.log('http://localhost:8080/counts/genres');
+    console.log('http://localhost:8080/counts/artists');
+    console.log('http://localhost:8080/counts/topgenres/20');
 
 });
